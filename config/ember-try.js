5888276f7eb6e97beb7e04760923d73bf5aeb350
Lawrence Davis <lawrence@lrdiv.co>
2020-07-24 21:14:39 -0500

    Add ember-3.5 to ember-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,6 +6,14 @@ module.exports = async function() {
   return {
     scenarios: [
       {
+        name: 'ember-3.5',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.5.0'
+          }
+        }
+      },
+      {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
