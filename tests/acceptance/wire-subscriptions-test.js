7037725dd680b998550a31241b04d665b08e5f0e
Lawrence Davis <lawrence@lrdiv.co>
2020-08-04 16:42:58 -0500

    Don't run glimmer tests against classic ember editions

--- tests/acceptance/wire-subscriptions-test.js
+++ tests/acceptance/wire-subscriptions-test.js
@@ -7,7 +7,7 @@ module('Acceptance | wire subscriptions', function(hooks) {
 
   setupApplicationTest(hooks);
 
-  test('#wireSubscriptions on a route', async function(assert) {
+  test('#wireSubscriptions on a route | classic', async function(assert) {
     const pusherService = this.owner.lookup('service:pusher');
     pusherService.client = new PusherMock();
 
@@ -17,18 +17,25 @@ module('Acceptance | wire subscriptions', function(hooks) {
     assert.ok(pusherService.client.channels['channel-2'], 'should subscribe to the proper channel');
     assert.equal(findAll('[data-test-message]').length, 0, 'should not show any messages on load');
 
-    assert.ok(pusherService.client.channels['user-list'], 'should subscribe from the glimmer component');
-    assert.equal(findAll('[data-test-user]').length, 0, 'should not show any users on load');
-
     // Emit a message to the event the route subscribed to using pusher mock
     pusherService.bindings['channel-2'].channel.emit('new-message', { author: 'Tomster', body: 'Ember.js is very cool!', timestamp: Date.now() });
     assert.equal(findAll('[data-test-message]').length, 1, 'should append a message from pusher');
+  });
+
+  test('#wireSubscriptions on a glimmer component', async function(assert) {
+    const pusherService = this.owner.lookup('service:pusher');
+    pusherService.client = new PusherMock();
+
+    await visit('/chat/channel-3');
+    assert.equal(currentURL(), '/chat/channel-3');
+
+    assert.ok(pusherService.client.channels['user-list'], 'should subscribe from the glimmer component');
+    assert.equal(findAll('[data-test-user]').length, 0, 'should not show any users on load');
 
     pusherService.bindings['user-list'].channel.emit('user-arrive', { id: 1, name: 'Yehuda' });
     assert.equal(findAll('[data-test-user]').length, 1, 'should append a user from pusher in glimmer component');
 
     pusherService.bindings['user-list'].channel.emit('user-leave', { id: 1, name: 'Yehuda' });
     assert.equal(findAll('[data-test-user]').length, 0, 'should remove a user from pusher in glimmer component');
-
   });
 });
--- tests/unit/services/pusher-test.js
+++ tests/unit/services/pusher-test.js
@@ -3,7 +3,7 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { PusherChannelMock } from 'pusher-js-mock';
 
-module('Unit | Service | pusher', function(hooks) {
+module('Unit | Service | pusher | classic', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
